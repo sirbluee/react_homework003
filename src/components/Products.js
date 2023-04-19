29d49c9627d1c64c7e4e51fa59665ec505c549c8
sophe4rum <sohearum14@gmail.com>
2023-04-19 23:59:32 +0700

    sorn_sophearum

--- src/components/Products.js
+++ src/components/Products.js
@@ -32,13 +32,14 @@ export default function Products() {
       ))}
 
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top"  src="https://cdn.dribbble.com/users/897154/screenshots/3763556/media/772c32922786c7173644fb7173563ea1.gif"  />
         <Card.Body>
           <Placeholder as={Card.Title} animation="glow">
             <Placeholder xs={6} />
           </Placeholder>
           <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{" "}
+            <Placeholder xs={7} /> <Placeholder xs={4} /> 
+            <Placeholder xs={4} />{" "}
             <Placeholder xs={6} /> <Placeholder xs={8} />
           </Placeholder>
           <Placeholder.Button variant="primary" xs={6} />
