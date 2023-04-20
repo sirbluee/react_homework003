a78a67233ac7c21d92ee4d18470e1b763864274e
sophe4rum <sohearum14@gmail.com>
2023-04-20 08:11:56 +0700

    sorn__sophearum

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,10 +14,33 @@ export default function Products() {
         setProduct(data);
       });
   };
+  console.log(products.length);
   useEffect(() => {
     fetchProduct();
   }, []);
 
+  // loop placeholder
+  let placeholder = [];
+  for (let i = 0; i < 225; i++) {
+    placeholder.push(
+      <Card className="d-inline-flex m-3" style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src="https://cdn.dribbble.com/users/897154/screenshots/3763556/media/772c32922786c7173644fb7173563ea1.gif"
+        />
+        <Card.Body>
+          <Placeholder as={Card.Title} animation="glow">
+            <Placeholder xs={6} />
+          </Placeholder>
+          <Placeholder as={Card.Text} animation="glow">
+            <Placeholder xs={7} /> <Placeholder xs={4} />
+            <Placeholder xs={4} /> <Placeholder xs={6} /> <Placeholder xs={8} />
+          </Placeholder>
+          <Placeholder.Button variant="primary" xs={6} />
+        </Card.Body>
+      </Card>
+    );
+  }
   return (
     <div className="container">
       {products.map((product) => (
@@ -31,20 +54,8 @@ export default function Products() {
         </Card>
       ))}
 
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top"  src="https://cdn.dribbble.com/users/897154/screenshots/3763556/media/772c32922786c7173644fb7173563ea1.gif"  />
-        <Card.Body>
-          <Placeholder as={Card.Title} animation="glow">
-            <Placeholder xs={6} />
-          </Placeholder>
-          <Placeholder as={Card.Text} animation="glow">
-            <Placeholder xs={7} /> <Placeholder xs={4} /> 
-            <Placeholder xs={4} />{" "}
-            <Placeholder xs={6} /> <Placeholder xs={8} />
-          </Placeholder>
-          <Placeholder.Button variant="primary" xs={6} />
-        </Card.Body>
-      </Card>
+      {/* Placeholder */}
+      {placeholder}
     </div>
   );
 }
